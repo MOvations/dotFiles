b9e39f57b0fbd9dfc3695c83fd98ae3a5d41d3ea
movations <mofreshflava@gmail.com>
2021-02-10 13:55:17 -0600

    Updated script and readme for cleaner use with Ubuntu server. Still need to add conditionals.

--- README.md
+++ README.md
@@ -1,22 +1,28 @@
-## Setup Windows Terminal in WSL2 on Ubuntu (18.04/20.04)
+# Setup Terminal in Ubuntu (18.04/20.04) with support for WSL2 and Ubuntu Server
 
-Motivation: <br>
-    - I really dislike setting up  environments, and I do it a lot. So I'm taking the time to make an install to get things the way I like. </br>
+## Motivation: <br>
+I dislike setting up environments, so I'm automating it to speed up deployments. I'll be trying to incorporate as many of my settingas as I like for all the linux environments I use.</br>
 
-Instructions:<br>
+### Contents
+* [Quickstart: Run and Done](#Quickstart-Run-and-Done)
+* [Thanks to](#thanks-to)
+* [Final Notes](#final-notes)
+
+## Quickstart: Run and Done 
 Setup WSL2 for your Windows 10 pro system and isntall an Ubuntu distro from the Microsoft store. 
 (Note: I've tested this on 20.04/18.04 so far)
-I also use the new Microsoft Terminal, which you also install from the micro soft store </br>
+I also use the new Microsoft Terminal, which you also install from the micro soft store
 
 It's still a bit quirky (sorry) sometime it installs in one pass, sometimes it only gets upto the 'git' portion
-if this happens run "code . " (without quotes) to launch VS Code and comment out the first part of the script. Also add the powerline10k to the .zshrc as well. Then rerun the modified script. Close the terminal reload, and boom. done. sometime this runs all at once, sometimes takes two passes. Still figuring out why...
+if this happens rerun the setup script
 
 
-Thanks to:
-    [Scott Hanselman](https://www.hanselman.com/blog/ItsTimeForYouToInstallWindowsTerminal.aspx)
-    and
-    [Fireship.io](https://fireship.io/lessons/windows-10-for-web-dev/)
-    for getting me setup with some great settings!
+## Thanks to:
+[Scott Hanselman](https://www.hanselman.com/blog/ItsTimeForYouToInstallWindowsTerminal.aspx)
+and
+[Fireship.io](https://fireship.io/lessons/windows-10-for-web-dev/)
+for getting me setup with some great settings!
 
-Note:
-I implement 2 terminal settings (bash and zsh) as VScode and Windows Terminal will behave differently
+## Final Notes:
+
+I left settings for bashrc (commented out) in the setup script if you'd like to have different look in an editor (e.g., VScode) vs Terminal
--- setup_WinTermWSL2.sh
+++ setup_WinTermWSL2.sh
@@ -1,52 +1,55 @@
 
 #! /bin/bash
 
+##### to make manual changes to bashrc, modify PS1 to something like this #####
 # echo 'export PS1="[\t] ubuntu20:\w\$ "' >> ~/.bashrc
 
+###### define basic promt colors for bash #####
+# cat <<EOF >> ~/.bashrc
+##### ANSI color codes #####
+# RS="\[\033[0m\]"    # reset
+# HC="\[\033[1m\]"    # hicolor
+# UL="\[\033[4m\]"    # underline
+# INV="\[\033[7m\]"   # inverse background and foreground
+# FBLK="\[\033[30m\]" # foreground black
+# FRED="\[\033[31m\]" # foreground red
+# FGRN="\[\033[32m\]" # foreground green
+# FYEL="\[\033[33m\]" # foreground yellow
+# FBLE="\[\033[34m\]" # foreground blue
+# FMAG="\[\033[35m\]" # foreground magenta
+# FCYN="\[\033[36m\]" # foreground cyan
+# FWHT="\[\033[37m\]" # foreground white
+# BBLK="\[\033[40m\]" # background black
+# BRED="\[\033[41m\]" # background red
+# BGRN="\[\033[42m\]" # background green
+# BYEL="\[\033[43m\]" # background yellow
+# BBLE="\[\033[44m\]" # background blue
+# BMAG="\[\033[45m\]" # background magenta
+# BCYN="\[\033[46m\]" # background cyan
+# BWHT="\[\033[47m\]" # background white
+# EOF
 
-# Make the basic promt look a little different
-cat <<EOF >> ~/.bashrc
-# ANSI color codes
-RS="\[\033[0m\]"    # reset
-HC="\[\033[1m\]"    # hicolor
-UL="\[\033[4m\]"    # underline
-INV="\[\033[7m\]"   # inverse background and foreground
-FBLK="\[\033[30m\]" # foreground black
-FRED="\[\033[31m\]" # foreground red
-FGRN="\[\033[32m\]" # foreground green
-FYEL="\[\033[33m\]" # foreground yellow
-FBLE="\[\033[34m\]" # foreground blue
-FMAG="\[\033[35m\]" # foreground magenta
-FCYN="\[\033[36m\]" # foreground cyan
-FWHT="\[\033[37m\]" # foreground white
-BBLK="\[\033[40m\]" # background black
-BRED="\[\033[41m\]" # background red
-BGRN="\[\033[42m\]" # background green
-BYEL="\[\033[43m\]" # background yellow
-BBLE="\[\033[44m\]" # background blue
-BMAG="\[\033[45m\]" # background magenta
-BCYN="\[\033[46m\]" # background cyan
-BWHT="\[\033[47m\]" # background white
 
+# if [ "\$color_prompt" = yes ]; then
+#     PS1='\${debian_chroot:+(\$debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\\$ '
+# else
+#     #PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\$ '
+#     PS1="\$HC\$FYEL[ \$FBLE\${debian_chroot:+(\$debian_chroot)}\u\$FYEL: \$FBLE\w \$FYEL]\\\$ \$RS"
+#     PS2="\$HC\$FYEL&gt; \$RS"
+# fi
+# EOF
 
-if [ "\$color_prompt" = yes ]; then
-    PS1='\${debian_chroot:+(\$debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\\$ '
-else
-    #PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\$ '
-    PS1="\$HC\$FYEL[ \$FBLE\${debian_chroot:+(\$debian_chroot)}\u\$FYEL: \$FBLE\w \$FYEL]\\\$ \$RS"
-    PS2="\$HC\$FYEL&gt; \$RS"
-fi
-EOF
-
-# install Oh My Zosh
+##### install Oh My Zosh #####
 sudo apt install -y zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" 
 sudo apt-get install -y fonts-powerline
-source ~/.zshrc
+echo "Oh My Zosh install, use source ~/.zshrc or logout then login to activate"
+# source ~/.zshrc
 
-# Make sure git is installed
+##### Make sure git is installed #####
 sudo apt install -y git
-# Set some git credentials
+
+##### Set some git credentials #####
 echo
 read -p 'git username: ' user_var
 git config --global user.name "$user_var"
@@ -54,14 +57,15 @@ read -p 'git email: ' email_var
 git config --global user.email "$email_var"
 read -sp 'git password: ' pass_var
 git config --global user.password "$pass_var"
-# cache these vars for 12 hours
+
+##### cache these vars for 12 hours #####
 git config --global credential.helper cache --timeout=43200
 
-# Use the power10k theme
+##### Use the power10k theme #####
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 sed -i 's|ZSH_THEME="robbyrussell"|ZSH_THEME="powerlevel10k/powerlevel10k"|' ~/.zshrc
 
-# Install Anaconda (miniconda), follow the prompts
+###### Install Anaconda (miniconda), follow the prompts #####
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh
 rm Miniconda3-latest-Linux-x86_64.sh
@@ -69,32 +73,42 @@ rm Miniconda3-latest-Linux-x86_64.sh
 echo " the current user is: $USER"
 export PATH=/home/$USER/miniconda3/bin:$PATH
 
-# set PATH so it includes miniconda's bin
+##### set PATH so it includes miniconda's bin #####
 cat <<EOF >> ~/.bashrc
 if [ -d "\$HOME/miniconda3/bin" ]; then
     PATH="\$HOME/miniconda3/bin:\$PATH"
 fi
 EOF
 
-# install jupyter-lab and set the browser(Firefox in my case) to open
+###### install jupyter-lab and set the browser(Firefox in my case) to open #####
 echo
-echo "##### Creating Conda Environment 'wsl' #####"
+echo "##### Creating Conda Environment 'py39' #####"
 echo
 conda update -y conda
-conda create -n wsl -y python=3.9
+conda create -n py39 -y python=3.9
 conda init zsh
-conda activate wsl
+conda activate py39
 conda install -y -c conda-forge jupyterlab
 conda install -y -c conda-forge nodejs
 conda install -y -c pyviz holoviz
 jupyter labextension install @pyviz/jupyterlab_pyviz
-echo "export BROWSER='/mnt/c/Program Files/Mozilla Firefox/firefox.exe'" >> ~/.zshrc
+
+echo
+echo "##### Generating Jupyter Lab config file at ~/.jupyter/jupyter_notebook_config.py #####"
+echo
 jupyter lab --generate-config
+
+
+echo
+echo "##### setting up bowser launch support for WSL - should make this an if statement #####"
+echo
+echo "export BROWSER='/mnt/c/Program Files/Mozilla Firefox/firefox.exe'" >> ~/.zshrc
 sed -i '/c.NotebookApp.use_redirect_file/s/^#//g' ~/.jupyter/jupyter_notebook_config.py
 sed -i 's| c.NotebookApp.use_redirect_file = True|c.NotebookApp.use_redirect_file = False|' ~/.jupyter/jupyter_notebook_config.py
 . ~/.profile
 
 echo
 echo "##### You're done!!! #####"
+echo "use source ~/.zshrc to launch the powerline10k setup"
 echo
 
