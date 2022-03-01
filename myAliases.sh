b12c4289e8057b2f8d146b6fa00f492f55868f4b
movations <mofreshflava@gmail.com>
2022-03-01 14:19:29 -0600

    updating alias and more

--- myAliases.sh
+++ myAliases.sh
@@ -49,3 +49,21 @@ fi
 ##### Launch Server #####
 alias servr="python3 -m http.server --bind localhost 7200 "
 
+##### Git aliases #####
+alias gs='git status'
+alias gc='git commit -am'
+alias gp='git pull --rebase'
+alias gl='git log --graph --pretty=format:"%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset" --abbrev-commit'
+git config --global credential.helper 'cache --timeout=30000' # cache git credentials for 8+ hrs
+
+##### mk function ####
+mk() { mkdir -p "$@" && cd "$_"; }
+
+##### Weather #####
+alias wttr="curl wttr.in"
+
+##### Stash the SSH Passphrase #####
+alias ssha='eval $(ssh-agent) && ssh-add'
+
+##### Custom alias to my updater file #####
+alias updater='bash ~/.scripts/updater.sh'
