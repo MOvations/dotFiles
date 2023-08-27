c3a27b27e3aed0c00e49d871a0c09554e20b8ee1
Mark H. Oliver <markholiver@gmail.com>
2023-08-27 17:31:12 -0500

    Updates based on comparing what works across all macs

--- setup_iterm.sh
+++ setup_iterm.sh
@@ -39,12 +39,31 @@ sed -i 's|ZSH_THEME="robbyrussell"|ZSH_THEME="powerlevel10k/powerlevel10k"|' ~/.
 
 # miniconda -- https://docs.conda.io/en/latest/miniconda.html
  
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-arm64.sh -O ~/miniconda.sh
-bash ~/miniconda.sh -b -p $HOME/miniconda
-rm -rf ~/miniconda
 
+# this manuall installs miniconda, brew can also do it, so I will do that 
+# wget https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-arm64.sh -O ~/miniconda.sh
+# bash ~/miniconda.sh -b -p $HOME/miniconda
+# rm -rf ~/miniconda
+
+brew install -cask miniconda # review docs to get conda init to work https://formulae.brew.sh/cask/miniconda#default
 brew install conda-zsh-completion
+brew install zsh-syntax-highlighting
+# add to .zshrc
+# plugins=(git zsh-syntax-highlighting zsh-autosuggestions)
+# had to  use this to get to work on 14" but not 16" for some reason...
+# '''
+# git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+# git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+# '''
+
+conda create -y -n py10 python=3.10 pandas matplotlib seaborn && conda activate py10 && conda install -y -c conda-forge jupyterlab pyarrow fastparquet
+pip install jupyterlab_theme_solarized_dark
+
 brew install zsh-autocomplete
-Brew install btop
+brew install btop
+brew install node
+
+# instant dock settings
+defaults write com.apple.Dock autohide-delay -float 0 && killall Dock
 
 
